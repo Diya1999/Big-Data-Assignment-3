8c6faffbcd31886089652177254a95081a36539f
PragnyaSridhar <43640958+PragnyaSridhar@users.noreply.github.com>
2019-10-29 21:27:49 +0530

    task-1 2nd

--- t1.py
+++ t1.py
@@ -1,36 +1,49 @@
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession,Row
 from pyspark.sql.functions import explode
-from pyspark.sql.functions import split
+from pyspark.sql.functions import split,flatten
 from pyspark.sql.types import StructType
 
-from pyspark import SparkConf,SparkContext
+from pyspark import SparkConf, SparkContext
 from pyspark.streaming import StreamingContext
-from pyspark.sql import Row,SQLContext
+from pyspark.sql import Row, SQLContext
+from pyspark.sql.types import Row
+from pyspark import SparkContext
+
+def process_row(row):
+    pass
 
 
-spark = SparkSession \
-  .builder \
-  .appName("CommonHash") \
-  .getOrCreate()
+spark = SparkSession.builder.appName("CommonHash").getOrCreate()
 
 
 # userSchema= StructType().add("word","string").add("id","integer")
-userSchema= StructType().add("n","integer").add("id","integer").add("Lang","string")\
-    .add("Date","string").add("Source","string").add("len","integer")\
-        .add("Likes","integer").add("RT's","string").add("Hashtags","string").\
-            add("UserMentionNames","string").add("UserMentionID","string").\
-                add("Name","string").add("Place","string").add("Followers","string").\
-                    add("Friends","string")
-    
+userSchema = (
+    StructType()
+    .add("n", "integer")
+    .add("id", "integer")
+    .add("Lang", "string")
+    .add("Date", "string")
+    .add("Source", "string")
+    .add("len", "integer")
+    .add("Likes", "integer")
+    .add("RT's", "string")
+    .add("Hashtags", "string")
+    .add("UserMentionNames", "string")
+    .add("UserMentionID", "string")
+    .add("Name", "string")
+    .add("Place", "string")
+    .add("Followers", "string")
+    .add("Friends", "string")
+)
 
-lines = spark \
-    .readStream \
-    .format("csv") \
-    .option("sep", ";") \
-    .schema(userSchema) \
+
+lines = (
+    spark.readStream.format("csv")
+    .option("sep", ";")
+    .schema(userSchema)
     .load("hdfs://localhost:9000/stream")
+)
 
-  
 
 # words = lines.select(
 #    explode(
@@ -39,14 +52,26 @@ lines = spark \
 # )
 
 
-# wordCounts = lines.map(lambda line:line[7].split(",")).map(lambda x:(x,1)).groupByKey().count()
-wordCounts=lines.groupBy("UserMentionID").count()
+wordCounts = lines.select(split(lines.Hashtags,",").alias("sp"))
+# wordCounts = flatten(wordCounts.sp).alias("F")
+
+wordCounts = flatten(wordCounts.sp)
 
 
-query = wordCounts \
-    .writeStream \
-    .outputMode("complete") \
-    .format("console") \
-    .start()
+# wordCounts = 
+#     .map(lambda word: (word, 1))
+#     .reduceByKey(add)
+# )
+# wordCounts = wordCounts.collect()
+
+
+# wordCounts=lines.groupBy("UserMentionID").count()
+
+# wordCounts=lines.select("Hashtags").count()
+
+# query = wordCounts.writeStream.foreach(process_row).start()
+# wordCounts = wordCounts.RDD
+
+query = wordCounts.writeStream.outputMode("update").format("console").start()
 
 query.awaitTermination()
