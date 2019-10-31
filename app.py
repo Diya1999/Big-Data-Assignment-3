0368c142810f11ca95e785d47f984ef74d766c34
Namrata R <37548876+Namratajjampur@users.noreply.github.com>
2019-10-31 22:13:58 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -17,11 +17,10 @@ def send_data_to_spark(tcp_connection,reader):
 		finstr=finstr+'\n'
 		count=count+1
 		tcp_connection.send(finstr.encode())
-		if(count==2000):
-			# tcp_connection.send(finstr.encode())
-			time.sleep(2)
+		if(count==500):
+			time.sleep(1)
 			count=0
-	# tcp_connection.close()
+	tcp_connection.close()
 
 TCP_IP="localhost"
 TCP_PORT=9009
@@ -30,15 +29,13 @@ s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
 s.bind((TCP_IP,TCP_PORT))	
 
-csvfile=open('bigdata.csv','r')
+csvfile=open('FIFA_modded_small.csv','r')
 fieldnames = ("ID","language","Date","source","len","likes","RTs","Hashtags","Usernames","Userid","name","Place","followers","friends")
 reader=csv.reader(csvfile,fieldnames)
-time.sleep(2)
-
+time.sleep(2)	
 s.listen(1)
 print("Waiting for connection...")
 
 conn,addr=s.accept()
-print("Connected... Starting to get twitter data")
-
+print("Connected... Starting to get fake data")
 send_data_to_spark(conn,reader)
--- spark.py
+++ spark.py
@@ -14,69 +14,23 @@ from pyspark.sql.functions import explode
 from pyspark.sql.functions import split
 from pyspark.sql.functions import window
 
-def aggregate_tweets_count(new_values, total_sum):
-	return sum(new_values) + (total_sum or 0)
-
-def get_sql_context_instance(spark_context):
-	if ('sqlContextSingletonInstance' not in globals()):
-		globals()['sqlContextSingletonInstance'] = SQLContext(spark_context)
-	return globals()['sqlContextSingletonInstance']
-
-def process_rdd(time, rdd):
-		print("----------=========- %s -=========----------" % str(time))
-		try:
-			sql_context = get_sql_context_instance(rdd.context)
-			row_rdd = rdd.map(lambda w: Row(tweetid=w[0], no_of_tweets=w[1]))
-			hashtags_df = sql_context.createDataFrame(row_rdd)
-			hashtags_df.registerTempTable("hashtags")
-			hashtag_counts_df = sql_context.sql("select tweetid, no_of_tweets from hashtags")
-			hashtag_counts_df.show()
-		except:
-			e = sys.exc_info()[0]
-			print("Error: %s" % e)
-
-def tmp(x):
-	return (x.split(';')[0],1)
 
 if __name__ == "__main__":
+	
+	window_size, batch_size=sys.argv[1],sys.argv[2]
 	conf=SparkConf()
 	conf.setAppName("BigData")
 	sc=SparkContext(conf=conf)
 
-	ssc=StreamingContext(sc,2)
-	#ssc.checkpoint("/checkpoint_BIGDATA")
+	ssc=StreamingContext(sc,int(batch_size))
+	ssc.checkpoint("/home/kishan/Downloads/checkpoint")
 	lines=ssc.socketTextStream("localhost",9009)
 	words = lines.map(lambda line: line.split(";")[7])
 	words = words.map(lambda x:x.split(','))
 	hashtag=words.flatMap(lambda x:(x,1))
-	windowedWordCounts = hashtag.reduceByKeyAndWindow(lambda x, y: x + y, 20, 2)
+	windowedWordCounts = hashtag.reduceByKeyAndWindow(lambda x, y: x + y,lambda x, y:x-y,int(window_size),1 )
 	windowedWordCounts.pprint()
-	#dataStream.pprint()
-	#tweet=dataStream.map(tmp)'''
-	'''lines = spark\
-        .readStream\
-        .format('socket')\
-        .option('localhost', host)\
-        .option('9009', port)\
-        .option('includeTimestamp', 'true')\
-        .load()'''
-
-	# Split the lines into words, retaining timestamps
-    # split() splits each line into an array, and explode() turns the array into multiple rows
 	
-		# OR
-		#tweet=dataStream.map(lambda w:(w.split(';')[0],1))
-		#tweet.pprint()
-		#count=tweet.reduceByKey(lambda x,y:x+y)
-		#count.pprint()
-
-		#TO maintain state
-		 #totalcount=tweet.updateStateByKey(aggregate_tweets_count)
-		 #totalcount.pprint()
-
-		#To Perform operation on each RDD
-		 #totalcount.foreachRDD(process_rdd)
-
 	ssc.start()
-	ssc.awaitTermination(12)
+	ssc.awaitTermination(25)
 	ssc.stop()
